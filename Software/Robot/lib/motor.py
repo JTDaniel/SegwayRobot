fa5a69e450bb8f6008278a5b3d1c36830201713e
Travis D. <trvsdaniel@gmail.com>
2024-03-20 22:49:32 -0700

    Added CAD Models, Motors react to inclination, created motor object

---
+++ Software/Robot/lib/motor.py
@@ -0,0 +1,44 @@
+##################################################
+#Class file to define motor object and functions
+##################################################
+from machine import Pin, PWM
+
+ON = 1
+OFF = 0
+
+class Motor():
+    def __init__(self, In1, In2, En) -> None:
+
+        self.MAX_FREQ = 65025
+        self.MIN_FREQ = 0 
+        self.P_In1 = Pin(In1, .Pin.OUT)
+        self.P_In2 = Pin(In2, Pin.OUT)
+        self.P_En = PWM(Pin(En))
+        self.P_En.freq(1000)
+
+
+    def DriveForward(self, Speed) -> None:
+        self.P_In1.value(ON)
+        self.P_In2.value(OFF)
+        self.P_En(Speed)
+
+    def DriveReverse(self, Speed) -> None:
+        self.P_In1.value(OFF)
+        self.P_In2.value(ON)
+        self.P_En(Speed)
+
+    def Stop(self) -> None:
+        self.P_In1.value(OFF)
+        self.P_In2.value(OFF)
+        self.P_En(self.MIN_FREQ)
+
+    def EncoderCount() -> None:
+        pass
+
+
+    #Add ability to move by encoder counts.
+    def MoveRelative() -> None:
+        pass
+
+
+    
--- Software/Robot/main.py
+++ Software/Robot/main.py
@@ -6,10 +6,39 @@ LED.on()
 from imu import MPU6050
 from time import sleep
 from machine import Pin, I2C
+from math import atan2, degrees
+from motor import Motor
+
+
+##################################################
+#INPUT/OUTPUT MAPPING
+##################################################
+MOTORLEFT_IN_1 = 27
+MOTORLEFT_IN_2 = 28
+MOTORLEFT_EN = 13
+
+MOTORRIGHT_IN_1 = 22
+MOTORRIGHT_IN_2 = 26
+MOTORRIGHT_EN = 15
+
+
+
+
+
+def vector_2_degrees(x, y):
+    angle = degrees(atan2(y, x))
+    if angle < 0:
+        angle += 360
+    return angle
+
+
 
 
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 imu = MPU6050(i2c)
+
+MotorLeft = Motor(MOTORLEFT_IN_1, MOTORLEFT_IN_2, MOTORLEFT_EN)
+MotorRight = Motor(MOTORRIGHT_IN_1, MOTORRIGHT_IN_2, MOTORRIGHT_EN)
 
 
 while True:
@@ -20,5 +49,16 @@ while True:
     gy=round(imu.gyro.y)
     gz=round(imu.gyro.z)
 
+    XZ = vector_2_degrees(ax, az)
+    YZ =  vector_2_degrees(ay, az)
 
-    print(f"Angle is ({gx}, {gy}, {gz})")
+
+    if XZ < 90:
+        MotorLeft.DriveForward(MotorLeft.MAX_FREQ)
+        MotorRight.DriveReverse(MotorRight.MAX_FREQ)
+    elif XZ > 90:
+        MotorLeft.DriveReverse(MotorLeft.MAX_FREQ)
+        MotorRight.DriveForward(MotorRight.MAX_FREQ)
+
+
+    print(f"Angle is ({XZ}, {YZ})")
